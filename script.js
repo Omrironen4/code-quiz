de2c89987c03524367feecace55be7ca46204085
Omri <omri.ronen4@gmail.com>
2021-04-08 23:37:06 -0700

    added timer and finished window 1 css

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+var timeEl = document.querySelector(".time");
+var startBtn = document.querySelector(".startBtn")
 
 
+var startClick = startBtn.addEventListener("click", function () {
+    var count = 100;
+    var timer = setInterval(function countDown() {
+        timeEl.innerHTML = (`time: ${count}`);
 
+        count --; 
+        if (count ===0) {
+            clearInterval(timer);
+            timeEl.innerHTML = "time is up!"
+        }
+    },1000)
+})
 
-
-
-function example() {
-    var a = 10;
-    return a;
-}
-
-console.log(example())
